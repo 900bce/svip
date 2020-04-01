281ebf586af6fcb8ffc9b264277cb3ce59dc8018
Jimmy Wen <jimmywen@softmobile.com.tw>
2020-04-01 10:24:20 +0800

    modify addToBookmark function

--- js/index.js
+++ js/index.js
@@ -7,14 +7,26 @@ $(function () {
   })
 
   $(".bookmark-link-btn").click(function () {
+    // if (document.all) {
+    //   window.external.addFavorite($(this).attr('url'));
+    // } else if (window.sidebar) {
+    //   this.href = $(this).attr('url');
+    //   this.rel = "sidebar";
+    // } else {
+    //   alert('浏览器不支持加入收藏');
+    //   return false();
+    // }
+    console.log($(this).attr('url'))
     if (document.all) {
-      window.external.addFavorite($(this).attr('url'));
+      try {
+        window.external.addFavorite($(this).attr('url'), '开元棋牌SVIP版');
+      } catch (e) {
+        alert('浏览器不支持加入收藏');
+      }
     } else if (window.sidebar) {
-      this.href = address;
-      this.rel = "sidebar";
+      window.sidebar.addPanel('开元棋牌SVIP版', $(this).attr('url'), "");
     } else {
       alert('浏览器不支持加入收藏');
-      return false();
     }
   });
 
